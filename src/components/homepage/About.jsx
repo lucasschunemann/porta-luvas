07f1ceb5cb108103876434b79d0ac8bc0ce9862f
Lucas <lucas.vhschunemann@gmail.com>
2024-01-15 14:30:26 -0300

    alterações-navbar

--- src/components/homepage/About.jsx
+++ src/components/homepage/About.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import profileImg from "/src/assets/images/eulucas.jpeg";
+import profileImg from "/src/assets/images/eulucas1.jpeg";
 import { ScrollTrigger } from "gsap/all";
 import { gsap } from "gsap";
 import Heading from "../ui/Heading";
